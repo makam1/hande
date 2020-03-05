a8908be8eae2d92c36d98aca72f397a186a21b67
«makam1» <mairamaissata@gmail.com>
2020-03-05 12:51:26 +0000

    home page

--- agenda/lib/models/login.dart
+++ agenda/lib/models/login.dart
@@ -9,40 +9,39 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 
 
-class Login{
-// class Login extends StatefulWidget{
-//   @override
-//   State createState()=> new LoginState();
-// }
+class Login extends StatefulWidget{
+  @override
+  State createState()=> new LoginState();
+}
 
-// class LoginState extends State<Login> with SingleTickerProviderStateMixin{
-//   AnimationController _logoAnimationController;
-//   Animation<double>   _logoAnimation;
+class LoginState extends State<Login> with SingleTickerProviderStateMixin{
+  AnimationController _logoAnimationController;
+  Animation<double>   _logoAnimation;
 
-//   @override
-//   void initState(){
-//     super.initState();
-//     _logoAnimationController= new AnimationController(
-//       vsync: this,
-//       duration: new Duration(milliseconds:2000)
-//       );
-//     _logoAnimation= new CurvedAnimation(
-//       parent: _logoAnimationController,
-//       curve: Curves.bounceIn);
-//     _logoAnimation.addListener(()=>this.setState((){}));
-//     _logoAnimationController.forward();
-//   }
+  @override
+  void initState(){
+    super.initState();
+    _logoAnimationController= new AnimationController(
+      vsync: this,
+      duration: new Duration(milliseconds:2000)
+      );
+    _logoAnimation= new CurvedAnimation(
+      parent: _logoAnimationController,
+      curve: Curves.bounceIn);
+    _logoAnimation.addListener(()=>this.setState((){}));
+    _logoAnimationController.forward();
+  }
 
-  // TextEditingController username = new TextEditingController();
-  // TextEditingController password= new TextEditingController();
+  TextEditingController username = new TextEditingController();
+  TextEditingController password= new TextEditingController();
 
   String msg='';
   Future<List> _login() async {
     final response= await http.post("https://cacc4532.ngrok.io/api/login",headers:{
       'Accept': 'application/json',
     }, body: {
-      // "username":username.text,
-      // "password":password.text,
+      "username":username.text,
+      "password":password.text,
     });
     if(response.statusCode==200){
 
@@ -70,80 +69,80 @@ class Login{
     }
   }
 
-  // @override
-  // Widget build(BuildContext context){
-  //   return new Scaffold(
-  //     backgroundColor: Colors.white,
-  //     body: new Stack(
-  //       fit: StackFit.expand,
-  //       children: <Widget>[
-  //         new Column(
-  //           mainAxisAlignment: MainAxisAlignment.center,
-  //           children: <Widget>[
-  //             new Image(
-  //               image: new AssetImage("assets/calendar.png"),
-  //               height: _logoAnimation.value*99,
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      body: new Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Image(
+                image: new AssetImage("assets/calendar.png"),
+                height: _logoAnimation.value*99,
                 
-  //             ),
-  //             new Form(
-  //               child: new Container(
-  //                 padding: const EdgeInsets.only(left:50.0,right: 50.0),
-  //                 child: new Column(
-  //                 crossAxisAlignment: CrossAxisAlignment.center,
-  //                 children:<Widget> [
-  //                 new TextFormField(
-  //                   controller: username,
-  //                 decoration: new InputDecoration(
-  //                   labelText:"nom d'utilisateur", 
-  //                 ),
-  //                 keyboardType: TextInputType.text,
-  //                 ),
-  //                 new TextFormField(
-  //                   controller: password,
-  //                 decoration: new InputDecoration(
-  //                   labelText:"mot de passe", 
-  //                 ),
-  //                 keyboardType: TextInputType.text,
-  //                 obscureText: true,
-  //               ),
-  //               new Container(
-  //                 padding: const EdgeInsets.only(left:80.0,top: 10.0),
-  //                 child: new Text(
-  //                   "Mot de passe oublié?",
-  //                   style: TextStyle(color:Colors.red),
+              ),
+              new Form(
+                child: new Container(
+                  padding: const EdgeInsets.only(left:50.0,right: 50.0),
+                  child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children:<Widget> [
+                  new TextFormField(
+                    controller: username,
+                  decoration: new InputDecoration(
+                    labelText:"nom d'utilisateur", 
+                  ),
+                  keyboardType: TextInputType.text,
+                  ),
+                  new TextFormField(
+                    controller: password,
+                  decoration: new InputDecoration(
+                    labelText:"mot de passe", 
+                  ),
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                ),
+                new Container(
+                  padding: const EdgeInsets.only(left:80.0,top: 10.0),
+                  child: new Text(
+                    "Mot de passe oublié?",
+                    style: TextStyle(color:Colors.red),
 
-  //                 ),  
-  //               ),
-  //               new Padding(
-  //               padding: const EdgeInsets.only(left:85.0,top: 10.0),
+                  ),  
+                ),
+                new Padding(
+                padding: const EdgeInsets.only(left:85.0,top: 10.0),
 
-  //               ),
-  //               new MaterialButton(
-  //                 color: Colors.grey,
-  //                 textColor: Colors.black,
-  //                 child: new Text(
-  //                   "Connexion",
-  //                 ),
-  //                 onPressed:()  {
-  //                  _login();
-  //                 },
-  //                 splashColor: Colors.black,
-  //                 ),
-  //                 Text(msg),
+                ),
+                new MaterialButton(
+                  color: Colors.grey,
+                  textColor: Colors.black,
+                  child: new Text(
+                    "Connexion",
+                  ),
+                  onPressed:()  {
+                   _login();
+                  },
+                  splashColor: Colors.black,
+                  ),
+                  Text(msg),
 
-  //                 new Container(
-  //                 padding: const EdgeInsets.only(top:10.0),
-  //                 child: new Text(
-  //                   "Pas de compte? S'inscrire",
-  //                   style: TextStyle(color:Colors.red),
-  //                 ), 
-  //               )
-  //                 ])
-  //             )
-  //           )],
-  //         )],
-  //      ),
-  //   );
-  // }
+                  new Container(
+                  padding: const EdgeInsets.only(top:10.0),
+                  child: new Text(
+                    "Pas de compte? S'inscrire",
+                    style: TextStyle(color:Colors.red),
+                  ), 
+                )
+                  ])
+              )
+            )],
+          )],
+       ),
+    );
+  }
 }
 
--- agenda/lib/widgets/home.dart
+++ agenda/lib/widgets/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'dart:async';
+import 'package:Hande/models/login.dart';
+import 'package:date_format/date_format.dart';
 
 
 class Home extends StatefulWidget {
@@ -13,12 +15,80 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home>{
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+  static DateTime date= new DateTime.now();
+  String today =formatDate(new DateTime.now(), [dd, ' ',M, ' ', yyyy]);
+
+  @override
   Widget build(BuildContext context) {
     return new Scaffold(
      appBar: new AppBar(
-       title: new Text(widget.title)
-     ),
-     body: new Center(),    
-    );
-  }
+      
+      title:new  Text(
+        today,
+        style: TextStyle(fontSize: 15.0,fontStyle:FontStyle.italic,color: Colors.grey ),
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: <Color>[
+              Colors.red[500],
+              Colors.yellow[300]
+            ]))),         
+      leading: GestureDetector(
+          onTap: () { /* Write listener code here */ },
+          child: Icon(
+            Icons.home,  // add custom icons also
+          ),
+      ),
+      actions: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(right: 20.0),
+      child: GestureDetector(
+        onTap: () {},
+        child: Icon(
+          Icons.search,
+          size: 26.0,
+        ),
+      )
+    ),
+     Padding(
+          padding: EdgeInsets.only(right: 20.0),
+      child: GestureDetector(
+        onTap: () {},
+        child: Icon(
+          Icons.notifications,
+          size: 26.0,
+        ),
+      )
+    ),
+    Padding(
+      padding: EdgeInsets.only(right: 20.0),
+      child: GestureDetector(
+        onTap: () {},
+        child: Icon(
+            Icons.more_vert
+        ),
+            )
+          ),
+        ],
+      
+      ),  
+      body:login(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context){
+            return Login();
+          }));
+        },
+      ),
+                );
+        }
+      
+        login() {}    
 }
